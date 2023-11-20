26acf0fa830bd1fda6e3a87fb85b5a9889acacf0
Lance <lyf961336122@163.com>
2023-11-20 09:59:27 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,16 +31,17 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <script type="text/javascript">
+    
+        <meta name="config" content={JSON.stringify(getClientConfig())} />
+        <link rel="manifest" href="/site.webmanifest"></link>
+        <script src="/serviceWorkerRegister.js" defer></script>
+            <script type="text/javascript">
     (function(c,l,a,r,i,t,y){
         c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
         t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
         y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
     })(window, document, "clarity", "script", "jt82g9e47m");
 </script>
-        <meta name="config" content={JSON.stringify(getClientConfig())} />
-        <link rel="manifest" href="/site.webmanifest"></link>
-        <script src="/serviceWorkerRegister.js" defer></script>
       </head>
       <body>{children}</body>
     </html>
